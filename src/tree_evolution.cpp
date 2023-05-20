4c24db3e518ab88ede244be84f12ca3f7e35e640
Nefedov <nif06@mail.ru>
2023-05-20 19:19:01 +0300

    World seed and steps count from command line arguments

--- src/tree_evolution.cpp
+++ src/tree_evolution.cpp
@@ -1,31 +1,34 @@
 #include "world.h"
 
 #include <chrono>
+#include <cstdlib>
 
 namespace chr = std::chrono;
 
 
-int main() {
-	int w_i = 765;
-	while (true) {
-		std::cout << "w_i " << w_i << std::endl;
-		auto begin = chr::steady_clock::now();
-		World w(w_i);
-		++w_i;
-		while(w.current_step() < 5*60*60*30) {
-//		while(w.current_step() < 2'000'000) {
-//		while(w.current_step() < 30'000) {
-			w.proceed_step();
-			if (! w.check_life_exist()) {
-				std::cout << "All died\n";
-				break;
-			}
-		}
-		auto end = chr::steady_clock::now();
-		std::cout << chr::duration_cast<chr::seconds>(end - begin).count() << " seconds\n";
-		std::cout << "Done\n";
-		break;
+int main(int argc, char *argv[]) {
+	int w_i = chr::time_point_cast<chr::seconds>(chr::system_clock::now()).time_since_epoch().count();
+	if (argc > 1) {
+		w_i = std::atoi(argv[1]);
 	}
+	int steps = 5 * 60 * 60 * 30;
+	if (argc > 2) {
+		steps = std::atoi(argv[2]);
+	}
+
+	std::cout << "w_i " << w_i << std::endl;
+	auto begin = chr::steady_clock::now();
+	World w(w_i);
+	++w_i;
+	while(w.current_step() < steps && w.check_life_exist()) {
+		w.proceed_step();
+	}
+	if (! w.check_life_exist()) {
+		std::cout << "All died\n";
+	}
+	auto end = chr::steady_clock::now();
+	std::cout << chr::duration_cast<chr::seconds>(end - begin).count() << " seconds\n";
+	std::cout << "Done\n";
 
 	return 0;
 }
